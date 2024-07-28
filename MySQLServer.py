91563582909430ff9a0b89994d51ee3a48946e13
Arnold Adenya <aadenya7@gmail.com>
2024-07-28 23:45:43 +0300

    Discovering Databases

--- MySQLServer.py
+++ MySQLServer.py
@@ -1,14 +1,30 @@
 import mysql.connector
-from mysql.connector import errorcode
+from mysql.connector import Error
 
-# Database name
+
+# Name of the database to be created
 db_name = 'alx_book_store'
 
-def create_database(cursor):
+def create_database():
     try:
+        # Connect to MySQL server
+        connection = mysql.connector.connect(**config)
+        cursor = connection.cursor()
+        
+        # Create database if it doesn't exist
         cursor.execute(f"CREATE DATABASE IF NOT EXISTS {db_name}")
-    except mysql.connector.Error as err:
-        print(f"Failed creating database: {err}")
-    else:
         print(f"Database '{db_name}' created successfully or already exists!")
+    
+    except Error as e:
+        print(f"Error: {e}")
+    
+    finally:
+        # Close cursor and connection
+        if cursor:
+            cursor.close()
+        if connection:
+            connection.close()
+
+# Run the function to create the database
+create_database()
 
